bb325879a3c3f8678e66cf61debcbcd7f4461102
2400031802 <2400031802@kluniversity.in>
2026-04-28 09:11:05 +0530

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const BASE_URL = "https://food-backend-pb6h.onrender.com/food";
 
-const BASE_URL = "https://food-backend-pb6h.onrender.com";
+export const addFood = (data) => {
+  return axios.post(`${BASE_URL}/add`, data);
+};
 
-export const addFood = (data) =>
-  axios.post(`${BASE_URL}/add`, data);
-
-export const getFood = () =>
-  axios.get(`${BASE_URL}/all`);
+export const getFood = () => {
+  return axios.get(`${BASE_URL}/all`);
+};
